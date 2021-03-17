0728b3fa144b13405e4624b970199cb02eb4248b
pavelhardzei <pavelgordei11@gmail.com>
2021-03-17 22:36:04 +0300

    modified web server

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,17 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.*;
 
 
 public class WebServer {
-
     private static final int PORT = 8080;
-
-    private static ConcurrentHashMap<Integer, Task> tasksMap;
-    public static LinkedBlockingQueue<Task> tasksQueue;
+    public static ConcurrentHashMap<Integer, Task> tasksMap;
+    public static LinkedBlockingQueue<Socket> tasksQueue;
     private static int threadNum = 4;
     ExecutorService pool = Executors.newCachedThreadPool();
 
@@ -25,73 +20,7 @@ public class WebServer {
                     }
                     while (true) {
                         Socket client = server.accept();
-
-                        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                        PrintWriter out = new PrintWriter(client.getOutputStream());
-
-                        String line = in.readLine();
-                        if(line == null) {
-                            continue;
-                        }
-                        // Start sending our reply, using the HTTP 1.1 protocol
-                        out.print("HTTP/1.1 200 \r\n"); // Version & status code
-                        out.print("Content-Type: text/plain\r\n"); // The type of data
-                        out.print("Connection: close\r\n"); // Will close stream
-                        out.print("\r\n"); // End of headers
-
-                        Request request = Request.parse(line);
-                        switch(request.type) {
-                            case GET:
-                            {
-                                switch(request.getKey()) {
-                                    case "id":
-                                    {
-                                        Task task = WebServer.tasksMap.get(Integer.parseInt(request.getValue()));
-                                        if(task != null) {
-                                            if (task.getStatus() != Task.Status.DONE) {
-                                                out.println("Status: " + task.getStatus().name());
-                                                out.flush();
-                                            }
-                                            else {
-                                                task.printResult(out);
-                                            }
-                                        }
-                                        else {
-                                            out.println("Error: No such id found");
-                                            out.flush();
-                                        }
-                                        break;
-                                    }
-                                }
-                                break;
-                            }
-                            case POST:
-                            {
-                                Task task = null;
-                                switch(request.getKey()) {
-                                    case "countFactorial":
-                                    {
-                                        task = new Factorial(Integer.parseInt(request.getValue()));
-                                        break;
-                                    }
-                                    case "countPrime":
-                                    {
-                                        task = new Prime(Integer.parseInt(request.getValue()));
-                                        break;
-                                    }
-                                }
-                                if (task != null){
-                                    tasksMap.put(task.getID(), task);
-                                    tasksQueue.add(task);
-                                    out.println(task.getID());
-                                    out.flush();
-                                }
-                                break;
-                            }
-                        }
-                        in.close();
-                        out.close();
-                        client.close();
+                        tasksQueue.put(client);
                     }
                 }
                 catch(Exception e) {
