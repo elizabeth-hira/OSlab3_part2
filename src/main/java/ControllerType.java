1f0a60cd91c3bfb196d839731f9bd0d144b26753
pavelhardzei <pavelgordei11@gmail.com>
2021-03-19 19:20:40 +0300

    added annotations

--- src/main/java/ControllerType.java
+++ src/main/java/ControllerType.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Innokenty {
+public @interface ControllerType {
     Request.RequestType type();
     String name() default "";
 }
--- src/main/java/Controllers.java
+++ src/main/java/Controllers.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Controllers {
+public class Controllers {
+    private void executeTaskAsync(Task task) {
+        WebServer.tasksMap.put(task.getID(), task);
+        WebServer.poolTasks.execute(() -> {
+            task.setStatus(Task.Status.PROCESSING);
+            task.execute();
+            task.setStatus(Task.Status.DONE);
+        });
+    }
+
+    @ControllerType(type=Request.RequestType.POST, name="countFactorial")
+    public String countFactorial(Request request) {
+        Task task = new Factorial(Integer.parseInt(request.getValue()));
+        executeTaskAsync(task);
+        return String.valueOf(task.getID());
+    }
+    @ControllerType(type=Request.RequestType.POST, name="countPrime")
+    public String countPrime(Request request) {
+        Task task = new Prime(Integer.parseInt(request.getValue()));
+        executeTaskAsync(task);
+        return String.valueOf(task.getID());
+    }
+
+    @ControllerType(type=Request.RequestType.GET, name="id")
+    public String getInfo(Request request) {
+        Task task = WebServer.tasksMap.get(Integer.parseInt(request.getValue()));
+        if (task == null) return "Error: No such id found.";
+        if (task.getStatus() != Task.Status.DONE) return "Status: " + task.getStatus().name();
+        return task.response();
+    }
 }
